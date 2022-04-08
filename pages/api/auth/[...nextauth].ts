504a732aaa6cad2a75522c37f8198f8676fad702
Javier <51163175+jfa94@users.noreply.github.com>
2022-04-08 21:05:54 +0100

    session now returns token

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,6 +7,18 @@ export default NextAuth({
             clientId: process.env.COGNITO_CLIENT_ID,
             clientSecret: process.env.COGNITO_CLIENT_SECRET,
             issuer: `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${process.env.COGNITO_USER_POOL_ID}`,
+            idToken: true,
         })
-    ]
+    ],
+    callbacks: {
+        session: async function ({session, token}) {
+            session.user['sub'] = token.sub ?? session.sub
+            session.user['token'] = token.bearerToken ?? session.bearerToken
+            return session
+        },
+        async jwt({token, account}) {
+            token.bearerToken = account?.id_token ?? token.bearerToken;
+            return token;
+        },
+    },
 })
