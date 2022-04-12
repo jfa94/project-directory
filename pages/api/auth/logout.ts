c7fd507b210320de42869d2d527a73d26d86d1f7
Javier <51163175+jfa94@users.noreply.github.com>
2022-04-12 13:07:27 +0100

    added param types

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,10 +1,12 @@
-export default async function logout(req, res) {
+import {NextApiRequest, NextApiResponse} from "next"
+
+export default async function logout(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     if (req.method === 'GET') {
         try {
             await res.redirect(`https://${process.env.COGNITO_USER_POOL}.auth.${process.env.AWS_REGION}.amazoncognito.com/logout?client_id=${process.env.COGNITO_CLIENT_ID}&logout_uri=${process.env.CANONICAL_URL}/`);
         } catch (error) {
-            console.error(error);
-            res.status(error.status || 400).end(error.message);
+            console.error(error)
+            res.status(error.status || 400).end(error.message)
         }
     } else {
         res.status(405).end('Method not allowed')
