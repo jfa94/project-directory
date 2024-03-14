6d67a8656015bd4f3e37b0fe4387083922204118
Javier <51163175+jfa94@users.noreply.github.com>
2024-03-14 15:35:36 +0000

    Added more descriptive meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,20 +8,23 @@ export default class MyDocument extends Document {
                 <Head>
                     <meta charSet="utf-8"/>
                     <title>Uncovered: Achievement Tracker and Cover Letter Generator</title>
-                    <meta name="description"
-                          content="Track your projects and create custom cover letters using AI."/>
+                    <meta name="description" content="Achievement tracker and cover letter generator empowering professionals to effectively showcase their skills and experiences. Uncovered uses AI to create unique cover letters that address what recruiters are looking for. Stand out from other applicants, be better prepared for interviews, and make a bulletproof case for promotion."/>
+
                     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-                    <meta property="og:title"
-                          content="Uncovered: track your achievements and generate custom cover letters"/>
+                    <meta property="og:title" content="Uncovered: Achievement Tracker and Cover Letter Generator"/>
+                    <meta property="og:description" content="Uncovered helps showcase your skills and experience by keeping track of your achievements and using AI to generate unique cover letters." />
                     <meta property="og:type" content="website"/>
                     <meta property="og:url" content="https://uncoveredapp.com/"/>
                     {/*<meta property="og:image" content="img/indie-engineering-banner.png"/>*/}
-                    {/*<link rel="shortcut icon" href="/logo/favicon.ico"/>*/}
+                    <link rel="icon" href="logo/favicon.ico"/>
+
+                    <meta name="keywords" content="achievement tracking, cover letter generator, career development, job application, interview preparation, AI cover letters, promotion help"/>
+                    <meta name="author" content="Uncovered"/>
                 </Head>
                 <body>
-                <Main/>
-                <NextScript/>
+                    <Main/>
+                    <NextScript/>
                 </body>
             </Html>
         )
@@ -35,7 +38,7 @@ export default class MyDocument extends Document {
             ctx.renderPage = () =>
                 originalRenderPage({
                     enhanceApp: (App) => (props) =>
-                        sheet.collectStyles(<App {...props} />),
+                    sheet.collectStyles(<App {...props} />),
                 })
 
             const initialProps = await Document.getInitialProps(ctx)
